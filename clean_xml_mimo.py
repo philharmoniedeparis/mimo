46c9cf451d212b2c532b4f611195cf62388cf073
Marie Destandau <marie@ototoi.fr>
2015-02-20 14:41:28 +0100

    finds the numeric id from lexicon eid

--- clean_xml_mimo.py
+++ clean_xml_mimo.py
@@ -1,5 +1,9 @@
 # CLEANING THE XML EXPORT FROM IDESIA
 
+import re
+
+
+
 # opens the file, reads it then close it
 xmlFile = open("keywords.xml", "r")
 xmlContent = xmlFile.read()
@@ -8,14 +12,31 @@ xmlFile.close()
 # mends the closing tag (replaces <keywords/> with </keywords>)
 xmlContent = xmlContent.replace("<keywords/>", "</keywords>")
 
-
 # removes all Idesia nodes (but not their content)
 xmlContent = xmlContent.replace("<Idesia>", "")
 xmlContent = xmlContent.replace("</Idesia>", "")
 
 # removes all Thesaurus nodes (with their content)
-import re
 xmlContent = re.sub('<thesaurus>.*?</thesaurus>', '', xmlContent)
+
+
+
+
+#for each eid node
+eidNodes = re.findall('<eid>.*?</eid>', xmlContent)
+for eidNode in eidNodes:
+	print eidNode
+	# finds the numeric value
+	numericValue = eidNode.replace("<eid>LEXICON_", "")
+	numericValue = numericValue.replace("</eid>", "")
+	numericValue = int(numericValue)
+	# adds it as an attribute of the eid node
+	print numericValue
+	xmlContent = eidNode.replace(eidNode, "<eid id='" + str(numericValue) + "'>")
+	print eidNode
+	break
+
+print eidNodes
 
 # opens new file to write, puts the content and closes
 newXmlFile = open("keywords_cleaned.xml", "w")
